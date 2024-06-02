6e979e6f118683b5576ab0cd827103d517bc8a49
m-q000 <msamalq306@gmail.com>
2024-06-02 15:03:18 +0300

    urls.py & views.py updated

--- studentsapp/urls.py
+++ studentsapp/urls.py
@@ -4,7 +4,18 @@ from . import views
 
 urlpatterns = [
     path('',views.login ),
-    path('about/', views.about),
-    path('users/', views.users),
+    path('account/',views.account ),
+    path('accountAdmin/',views.accountAdmin ),
+    path('addCourse/',views.addCourse ),
+    path('addShedular/',views.addShedular ),
+    path('courseAdmin/',views.courseAdmin ),
+    path('courses/',views.courses ),
+    path('courseSchedular/',views.courseSchedular ),
+    path('coursesstudents/',views.coursesstudents ),
+    path('homeAdmin/',views.homeAdmin ),
+    path('register/',views.register ),
+    path('schedular/',views.schedular),
+    path('home/',views.home ),
+    
     
 ]
--- studentsapp/views.py
+++ studentsapp/views.py
@@ -4,7 +4,41 @@ from django.http import HttpResponse
 # Create your views here.
 def login(request):
     return render(request,"studentsapp/login.html")
-def users(request):
-    return HttpResponse("users page")
-def about(request):
-    return HttpResponse("about page")
+
+def home(request):
+    return render(request,"studentsapp/home.html")
+
+def account(request):
+    return render(request,"studentsapp/account.html")
+
+def accountAdmin(request):
+    return render(request,"studentsapp/accountAdmin.html")
+
+def addCourse(request):
+    return render(request,"studentsapp/addCourse.html")
+
+def addShedular(request):
+    return render(request,"studentsapp/addShedular.html")
+
+def courseAdmin(request):
+    return render(request,"studentsapp/courseAdmin.html")
+
+def courses(request):
+    return render(request,"studentsapp/courses.html")
+
+def courseSchedular(request):
+    return render(request,"studentsapp/courseSchedular.html")
+
+def coursesstudents(request):
+    return render(request,"studentsapp/coursesstudents.html")
+
+def homeAdmin(request):
+    return render(request,"studentsapp/homeAdmin.html")
+
+def register(request):
+    return render(request,"studentsapp/register.html")
+
+
+def schedular(request):
+    return render(request,"studentsapp/schedular.html")
+
