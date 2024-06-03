b8137d79e3535078e0e07f9337e17903403fc60f
Farahaf <201103@ppu.edu.ps>
2024-06-03 19:46:48 +0300

    note

--- studentsapp/urls.py
+++ studentsapp/urls.py
@@ -2,16 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('home/', views.home, name='home'),
-    path('register_course/', views.register_course, name='register_course'),
-    path('courses/', views.courses, name='courses'),
+    path('', views.login_view, name='login'),
     path('account/', views.account, name='account'),
     path('accountAdmin/', views.accountAdmin, name='accountAdmin'),
     path('addCourse/', views.addCourse, name='addCourse'),
     path('addShedular/', views.addShedular, name='addShedular'),
     path('courseAdmin/', views.courseAdmin, name='courseAdmin'),
+    path('courses/', views.courses, name='courses'),
+    path('courseSchedular/', views.courseSchedular, name='courseSchedular'),
     path('coursesstudents/', views.coursesstudents, name='coursesstudents'),
     path('homeAdmin/', views.homeAdmin, name='homeAdmin'),
+    path('register/', views.register, name='register'),
     path('schedular/', views.schedular, name='schedular'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+
 ]
