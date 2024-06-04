e463a8de37316fcd2e85672747d1006791736a79
Farahaf <201103@ppu.edu.ps>
2024-06-04 05:45:41 +0300

    fixed

--- courses/views.py
+++ courses/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import User
 from django.views.decorators.cache import cache_control
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+
 
 def courses(request):
     query = request.GET.get('search_query', '')
