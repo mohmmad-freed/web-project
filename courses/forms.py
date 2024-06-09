66ce7981a09d0600b9228677aa69d775e5362118
Mohammad_Fareed <mbdalmnm05@gmail.com>
2024-06-09 21:28:13 +0300

    show available times

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Course, CourseSchedule, Student
+from .models import *
 from django.contrib.auth.password_validation import validate_password, get_default_password_validators
 
 class StudentRegistrationForm(UserCreationForm):
@@ -33,6 +33,19 @@ class CourseForm(forms.ModelForm):
         model = Course
         fields = ['code', 'name', 'description', 'instructor', 'scheduled', 'prerequisites', 'capacity']
 
+    def __init__(self, *args, **kwargs):
+        super(CourseForm, self).__init__(*args, **kwargs)
+        self.fields['scheduled'].queryset = self.get_available_schedules()
+
+    def get_available_schedules(self):
+        # احصل على جميع الجداول الزمنية
+        schedules = CourseSchedule.objects.all()
+        available_schedules = CourseSchedule.objects.none()
+        for schedule in schedules:
+            # تحقق إذا كان هناك تعارض في الوقت والقاعة
+            if not Course.objects.filter(scheduled=schedule).exists():
+                available_schedules |= CourseSchedule.objects.filter(id=schedule.id)
+        return available_schedules
 class CourseScheduleForm(forms.ModelForm):
     class Meta:
         model = CourseSchedule
